cdc80696336d162f9faced986afe71e43f6bbad0
CodesEasy <info@codeseasy.com>
2026-04-02 00:02:24 +0530

    chore: add license headers to all source files

--- chain_test.go
+++ chain_test.go
@@ -1,3 +1,7 @@
+// Copyright 2026 CodesEasy. All rights reserved.
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
 package cron
 
 import (
